09630d061459e8bef76cc9b457e1ffeb02da7558
NWM04705 <nwm04705@hfsg.net>
2019-01-30 10:33:10 -0500

    Version base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'module-1', pathMatch: 'full'},
+  { path: 'module-1', loadChildren: './modulo-1/modulo-1.module#Modulo1Module'},
+  { path: 'module-2', loadChildren: './modulo-2/modulo-2.module#Modulo2Module'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
