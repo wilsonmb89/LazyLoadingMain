3571867435896cf80c6684adf828761e305819e4
NWM04705 <nwm04705@hfsg.net>
2019-01-31 13:37:19 -0500

    Version modulo 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'module-1', pathMatch: 'full'},
   { path: 'module-1', loadChildren: './modulo-1/modulo-1.module#Modulo1Module'},
-  { path: 'module-2', loadChildren: './modulo-2/modulo-2.module#Modulo2Module'}
+  { path: 'module-2', loadChildren: './modulo-2/modulo-2.module#Modulo2Module'},
+  { path: 'module-3', loadChildren: './modulo-3/modulo-3.module#Modulo3Module'}
 ];
 
 @NgModule({
